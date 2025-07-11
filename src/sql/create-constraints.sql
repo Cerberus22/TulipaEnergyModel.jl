75fe644d70976c11c7745320541561295b941158
Yurian Lagrand <Y.G.Lagrand@student.tudelft.nl>
2025-07-11 14:28:35 +0200

    fixing sql

--- src/sql/create-constraints.sql
+++ src/sql/create-constraints.sql
@@ -833,55 +833,6 @@ with sub as
     t_high.time_block_end,
 from
     asset_time_resolution_rep_period as atr
-    join
-    t_highest_assets_and_out_flows as t_high
-        on
-            atr.asset = t_high.asset
-            and atr.time_block_start = t_high.time_block_start
-            and atr.rep_period = t_high.rep_period
-    join asset
-        on
-            asset.asset = t_high.asset
-where
-    asset.type in ('producer', 'conversion')
-    and asset.unit_commitment = true
-    and asset.unit_commitment_method = 'su_sd_ramp_with_vars'
-    and asset.ramping = true
-order by
-    t_high.asset,
-    t_high.year,
-    t_high.rep_period,
-    t_high.time_block_start)
-)
-select
-    nextval('id') as id,
-    sorted.*
-from
-    sorted
-order by
-    sorted.asset,
-    sorted.year,
-    sorted.rep_period,
-    sorted.time_block_start
-;
-
-
-drop sequence id
-;
-
-create sequence id start 1
-;
-
-create table cons_su_ramp_vars_flow_diff as
-with sub as
-(select distinct
-    t_high.asset,
-    t_high.year,
-    t_high.rep_period,
-    t_high.time_block_start,
-    t_high.time_block_end,
-from
-    asset_time_resolution_rep_period as atr
     join t_highest_assets_and_out_flows as t_high
         on atr.asset = t_high.asset
         and atr.rep_period = t_high.rep_period
@@ -982,8 +933,6 @@ select
     nextval('id') as id,
     cons_su_ramp_vars_flow_upper_bound.*
 from cons_su_ramp_vars_flow_upper_bound
-;
-from sub
 ;
 
 drop sequence id
